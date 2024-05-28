630fdef63a2bff0490ccd9b79dab9180a68fa1e5
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-05-28 16:34:56 +0300

    chore: Update NextUIProvider to use router.push for navigation

--- src/providers.tsx
+++ src/providers.tsx
@@ -2,7 +2,9 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  const router = useRouter();
+  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
 }
