43fe3966c88a8317ced23f9821c0e1c0ed202010
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-05-28 16:35:52 +0300

    chore: Update NavbarUI to include user authentication logic and display

--- src/components/ui/NavbarUI.tsx
+++ src/components/ui/NavbarUI.tsx
@@ -1,3 +1,4 @@
+import { auth, signIn, signOut } from "@/auth";
 import {
   Navbar,
   NavbarBrand,
@@ -5,38 +6,68 @@ import {
   NavbarItem,
   Link,
   Button,
+  Avatar,
 } from "@nextui-org/react";
 import React from "react";
 
-const NavbarUI = () => {
+const NavbarUI = async () => {
+  const session = await auth();
+  const user = session?.user;
   return (
     <Navbar className=" bg-gradient-to-b from-purple-600 to-purple-500 text-white">
       <NavbarBrand>
         <h1 className="font-bold">NEXT AUTH PRISMA TRAINING</h1>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link className="text-white udner" href="#">
-            ADD POST
-          </Link>
-        </NavbarItem> |
-        <NavbarItem>
-          <Link className="text-white" href="#">
-            VIEW POSTS
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
+      {user && (
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            <Link className="text-white udner" href="/post/add">
+              ADD POST
+            </Link>
+          </NavbarItem>{" "}
+          |
+          <NavbarItem>
+            <Link className="text-white" href="/posts">
+              VIEW POSTS
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+      )}
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#" className="text-white">
-            Login
-          </Link>
+        <NavbarItem className="flex gap-2 items-center">
+          {!user && (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("google");
+              }}
+            >
+              <Button type="submit">Sign in</Button>
+            </form>
+          )}
+          {user && (
+            <>
+              <Avatar
+                name={user.name || ""}
+                src={user.image || ""}
+                showFallback
+              />
+              <span>{user.name}</span>
+            </>
+          )}
         </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} href="#">
-            Sign Up
-          </Button>
-        </NavbarItem>
+        {user && (
+          <NavbarItem>
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </NavbarItem>
+        )}
       </NavbarContent>
     </Navbar>
   );
