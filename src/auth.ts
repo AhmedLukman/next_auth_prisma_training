09473ae78a1f778eb1fbe77de5fe32f251ba9ca9
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-05-28 14:44:43 +0300

    chore: Update NextAuth configuration with Prisma adapter and Google provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,13 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import prisma from "./lib/prisma";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  // theme: {
+  //   logo: "/logo.svg",
+  //   brandColor: "#000000",
+  // }
+  adapter: PrismaAdapter(prisma),
+  providers: [Google],
 });
