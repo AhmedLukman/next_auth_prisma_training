3dd7f052beba61e55cf3075c22d9196238bf27f8
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-05-30 19:44:32 +0300

    chore: Update SubmitButton component to accept custom color prop

--- src/components/ui/SubmitButton.tsx
+++ src/components/ui/SubmitButton.tsx
@@ -1,13 +1,30 @@
 "use client";
 
-import { Button } from "@nextui-org/react";
+import { Button, ThemeColors } from "@nextui-org/react";
 import React, { PropsWithChildren } from "react";
 import { useFormStatus } from "react-dom";
 
-const SubmitButton = ({ children }: PropsWithChildren) => {
-  const {pending} = useFormStatus();
+const SubmitButton = ({
+  children,
+  color,
+}: PropsWithChildren & {
+  color:
+    | "default"
+    | "secondary"
+    | "primary"
+    | "success"
+    | "warning"
+    | "danger"
+    | undefined;
+}) => {
+  const { pending } = useFormStatus();
   return (
-    <Button type="submit" className=" self-end" color="secondary" isLoading={pending}>
+    <Button
+      type="submit"
+      className=" self-end"
+      color={color}
+      isLoading={pending}
+    >
       {children}
     </Button>
   );
