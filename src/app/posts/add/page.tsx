54f410c2b6aeaee6419af669c8f013eebeb7a2a9
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-05-30 18:23:25 +0300

    feat: Add AddPostPage component for adding new posts

---
+++ src/app/posts/add/page.tsx
@@ -0,0 +1,20 @@
+import { auth } from "@/auth";
+import AddPostForm from "@/components/ui/AddPostForm";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const AddPostPage = async () => {
+  const session = await auth();
+  const user = session?.user;
+  if (!user) redirect("/api/auth/signin?callbackUrl=/posts/add");
+  return (
+    <main>
+      <div className="max-w-lg mx-auto">
+        <h1 className="text-3xl mt-14 mb-8">Add a new post</h1>
+        <AddPostForm />
+      </div>
+    </main>
+  );
+};
+
+export default AddPostPage;
