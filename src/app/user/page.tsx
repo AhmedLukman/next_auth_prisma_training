39865d63cfdee68e513470475f472bd2bdc2d5c0
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-03 13:28:22 +0300

    refactor: Rename AdminPage component to ViewUsersPage and update related imports and redirects

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -5,19 +5,17 @@ import { redirect } from "next/navigation";
 import UsersTable from "@/components/ui/UsersTable";
 import { Role } from "@/lib/contants";
 
-const AdminPage = async () => {
-
-  const session = await auth()
-  const user = session?.user
-  if (!user) redirect("/api/auth/signin?callbackUrl=/admin");
-  if(user.role !== Role.Admin) redirect("/");
+const ViewUsersPage = async () => {
+  const session = await auth();
+  const user = session?.user;
+  if (!user) redirect("/api/auth/signin?callbackUrl=/users");
+  if (user.role !== Role.Admin) redirect("/");
 
   const users = await prisma.user.findMany({
     include: {
       Post: true,
     },
   });
-
   return (
     <main>
       <h1 className="mt-10 text-center text-xl mb-5 font-bold">Users</h1>
@@ -26,4 +24,4 @@ const AdminPage = async () => {
   );
 };
 
-export default AdminPage;
+export default ViewUsersPage;
