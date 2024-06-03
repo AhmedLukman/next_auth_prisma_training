7188e1f302852171742052e3eea38a47167fd1cb
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-03 17:56:26 +0300

    feat: Add Github provider to NextAuth configuration in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import prisma from "./lib/prisma";
 import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // theme: {
@@ -9,6 +10,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   //   brandColor: "#000000",
   // }
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [Google, Github],
   trustHost: true
 });
