b7794e39600703e055345877b13ffed67eceb1d0
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-04 18:00:12 +0300

    chore: Add useful comments

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -2,3 +2,6 @@ import { auth } from "@/auth";
 import { cache } from "react";
 
 export const cachedAuth = cache(auth);
+
+// Deduplicates request
+// Dont use it on server actions just server pages. We only wanna get cached version when we open our page
