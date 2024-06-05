d49c31b14ecf58f5b05eaf33befe3a43548d9cbc
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-05 07:15:03 +0300

    refactor: Update PostDropdown and UserDropdown components to use dynamic styling based on current pathname

--- src/components/ui/PostDropdown.tsx
+++ src/components/ui/PostDropdown.tsx
@@ -11,25 +11,36 @@ import {
   DropdownItem,
   Link,
 } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
 
 const PostDropdown = () => {
+  const pathname = usePathname();
   return (
     <Dropdown>
       <NavbarItem>
         <DropdownTrigger>
-          <Button
-            disableRipple
-            className="p-0 text-white bg-transparent data-[hover=true]:bg-transparent"
-          >
+          <Button disableRipple className="p-0 text-white bg-transparent">
             POSTS
           </Button>
         </DropdownTrigger>
       </NavbarItem>
       <DropdownMenu>
-        <DropdownItem as={Link} className="text-black" href="/posts/add">
+        <DropdownItem
+          as={Link}
+          className={
+            pathname === "/posts/add" ? "text-white bg-purple-500" : "text-black"
+          }
+          href="/posts/add"
+        >
           Add Post
         </DropdownItem>
-        <DropdownItem as={Link} className="text-black" href="/posts">
+        <DropdownItem
+          as={Link}
+          className={
+            pathname === "/posts" ? "text-white bg-purple-500" : "text-black"
+          }
+          href="/posts"
+        >
           View Posts
         </DropdownItem>
       </DropdownMenu>
--- src/components/ui/UserDropdown.tsx
+++ src/components/ui/UserDropdown.tsx
@@ -11,8 +11,10 @@ import {
   DropdownItem,
   Link,
 } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
 
 const UserDropdown = () => {
+  const pathname = usePathname();
   return (
     <Dropdown>
       <NavbarItem>
@@ -26,7 +28,13 @@ const UserDropdown = () => {
         </DropdownTrigger>
       </NavbarItem>
       <DropdownMenu>
-        <DropdownItem as={Link} className="text-black" href="/users">
+        <DropdownItem
+          as={Link}
+          className={
+            pathname === "/users" ? "text-white bg-purple-500" : "text-black"
+          }
+          href="/users"
+        >
           View Users
         </DropdownItem>
       </DropdownMenu>
