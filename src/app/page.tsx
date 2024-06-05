2dc15f0dbe6628511455fd24b4a1cc05867f38af
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-05 06:12:56 +0300

    refactor: Improve Home component user authentication message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,42 @@
 "use client";
-
 import { Role } from "@/lib/contants";
 import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const session = useSession();
-  const user = session.data?.user;
-  const isLoading = session.status === "loading";
-  const isAdmin = user?.role === Role.Admin;
+  const { data, status } = useSession();
+  const isLoading = status === "loading";
+  const isAdmin = data?.user.role === Role.Admin;
+
+  const renderUserStatus = () => {
+    if (isLoading) return <p>Loading...</p>;
+    if (!data?.user)
+      return (
+        <p>
+          Sign up to add and view posts. You can choose to get admin rights for
+          free so be sure to check it out!
+        </p>
+      );
+    if (isAdmin) return <p>You are an admin, enjoy the perks!</p>;
+    return (
+      <p>
+        You are authenticated and can add and view posts. Gain admin rights by
+        clicking on the &apos;Be Admin&apos; button on the navbar.
+      </p>
+    );
+  };
+
   return (
     <main className="flex items-center mt-32 flex-col gap-5">
       <h2 className="text-3xl">Hello and welcome!</h2>
-      <p className="max-w-prose text-center">
-        This project served as a valuable learning experience for me, where I
-        explored and mastered the use of Next Auth v5 for authentication,
-        together with prisma as an ORM to efficiently authenticate and store
-        data in an online database.
-      </p>
-      {isLoading && <p className="text-center">Loading...</p>}
-      {!user && !isLoading && (
+      <div className="text-center px-5 max-w-prose">
         <p>
-          Sign up in order to add and view posts. You can choose to get admin
-          rights for free so be sure to check it out!{" "}
+          This project served as a valuable learning experience for me, where I
+          explored and mastered the use of Next Auth v5 for authentication,
+          together with prisma as an ORM to efficiently authenticate and store
+          data in an online database.
         </p>
-      )}
-      {user && (
-        <p>
-          {!isAdmin &&
-            "You are authenticated and can add and view posts. You can gain admin rights as well by clicking on the 'Be Admin' button on the navbar."}
-          {isAdmin && "You are an admin, enjoy the perks!"}
-        </p>
-      )}
+        <div className="mt-2">{renderUserStatus()}</div>
+      </div>
     </main>
   );
 }
